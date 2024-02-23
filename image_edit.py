5531bfb0695d5c3c5447b02be0322f9ed92d3fa4
Joe Marshall <joe.marshall@nottingham.ac.uk>
2024-02-23 14:56:58 +0000

    typo

--- image_edit.py
+++ image_edit.py
@@ -191,7 +191,7 @@ cp -rf $SCRIPT_PATH/contents/home/* /home
         task_text+= create_wpa_supplicant(options)
 
     task_text+="\nnmcli conn reload\n"
-    task_text+="\chpasswd -e < /boot/firmware/userconf.txt\n"
+    task_text+="chpasswd -e < /boot/firmware/userconf.txt\n"
     task_script.write_text(task_text,newline="\n")
 
 def create_network_manager_connections(options):
